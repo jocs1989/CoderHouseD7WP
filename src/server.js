8840deac8b1c76afbf7c09a91caa65a8203ab6d9
jocs1989 <iqi.zamora@hotmail.com>
2022-10-04 20:53:17 -0500

    Se crea filtro y se modifica instancia del server

--- src/server.js
+++ src/server.js
@@ -1,40 +1,22 @@
-import {Server as HttpServer} from 'http';
-import   {Server as IOServer}    from 'socket.io';
-import  Productos from './routes/api/productos/productos.js'
-import express from 'express';
+import Filtro from "./filtro.js";
+import { Server as HttpServer } from "http";
+import { Server as IOServer } from "socket.io";
+import Productos from "./routes/api/productos/productos.js";
+import express from "express";
 
 const app = express();
 //Seccion de use
 app.use(express.json());
 
-app.use('/productos/',Productos );
-//proteccion de datos
-app.get('*' ,(req,res)=>{
-  console.log(req.method)
-  res.status(400).json({ error :`-2,descripcion:  ruta ${req.url} método ${req.method}  no implementada` });
-})
-app.post('*' ,(req,res)=>{
-  console.log(req.method)
-  res.status(400).json({ error :`-2,descripcion:  ruta ${req.url} método ${req.method}  no implementada` });
-})
-app.put('*' ,(req,res)=>{
-  console.log(req.method)
-  res.status(400).json({ error :`-2,descripcion:  ruta ${req.url} método ${req.method}  no implementada` });
-})
-app.delete('*' ,(req,res)=>{
-  console.log(req.method)
-  res.status(400).json({ error :`-2,descripcion:  ruta ${req.url} método ${req.method}  no implementada` });
-})
-
-
-
+app.use("/productos/", Productos);
+app.use("*", Filtro);
 
 
 //seccion inicio websokect
-const httpServer = new HttpServer(app)
-const io = new IOServer(httpServer)
+const httpServer = new HttpServer(app);
+const io = new IOServer(httpServer);
 
-const PORT =process.env.PORT||8080
+const PORT = process.env.PORT || 8080;
 const listener = httpServer.listen(PORT, function () {
   try {
     //integrantes.deleteAll()
