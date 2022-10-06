0ddae21e6cedb914b95bdbc1b483f3941091c6de
jocs1989 <iqi.zamora@hotmail.com>
2022-10-06 14:55:27 -0500

    Se mejor el carrito con GET

--- src/routes/api/carrito/carrito.js
+++ src/routes/api/carrito/carrito.js
@@ -1,73 +1,74 @@
+import Carritos from "./carritos.js";
 import Contenedora from "../productos/contenedora.js";
-import { fileURLToPath } from "url";
-import path from "path";
+import { fileURLToPath } from 'url';
+import path from 'path';
 const __filename = fileURLToPath(import.meta.url);
-const _dirname = path.dirname(_filename);
-class Carrito {
-  constructor() {
-    this.productos = 0;
-    this.contenedor = {};
-    this.total = 0;
-    this.articulos = new Contenedora(__dirname + "/../productos/articulos.txt");
-    this.carritos = new Carritos(__dirname + "/carrito.txt");
-  }
-  async setAddCarAsync(id, cantidad) {
-    try {
-      const object = await this.articulos.getById(id);
-      const precio = Number(object.precio);
-      const stock = Number(object.stock);
+const __dirname = path.dirname(__filename);
+class Carrito {                               constructor() {
+  this.productos = 0;
+  this.contenedor = {};
+  this.total = 0;
+  this.articulos = new Contenedora(__dirname+"/../productos/articulos.txt");
+  this.carritos = new Carritos(__dirname+"/carrito.txt");
+}
+async setAddCarAsync(id, cantidad) {
+  try {
+    const object = await this.articulos.getById(id);
+    const precio = Number(object.precio);
+    const stock = Number(object.stock);
 
-      console.log(cantidad);
+    console.log(cantidad)
 
-      if (Number(object.stock) >= cantidad) {
-        this.total = this.total + cantidad * precio;
-        object.stock = stock - cantidad;
-        await this.articulos.updateById(object);
-        object.cantidad = cantidad;
-        if (this.contenedor[object.id] === undefined) {
-          this.contenedor[Number(object.id)] = object;
-        } else {
-          let cantidadVieja = object.cantidad;
-          object.cantidad = cantidadVieja + cantidad;
-          this.contenedor[Number(object.id)].cantidad =
-            cantidadVieja + cantidad;
-          this.contenedor[Number(object.id)] = object;
-        }
+    if (Number(object.stock) >= cantidad) {
 
-        this.productos++;
-        console.log(this.contenedor);
-
-        return await this.carritos.save(this.contenedor);
+      this.total = this.total + cantidad * precio;
+      object.stock = stock - cantidad;
+      await this.articulos.updateById(object);
+      object.cantidad = cantidad;
+      if (this.contenedor[object.id] === undefined) {
+        this.contenedor[Number(object.id)] = object;
       } else {
-        console.log("lo sentimos no hay stock");
+
+        let cantidadVieja = object.cantidad;
+        object.cantidad=cantidadVieja+cantidad
+        this.contenedor[Number(object.id)].cantidad = cantidadVieja + cantidad;
+        this.contenedor[Number(object.id)] =object
       }
-    } catch (err) {
-      console.log(err);
+
+      this.productos++;
+      console.log(this.contenedor);
+
+      return await this.carritos.save(this.contenedor);
+    } else {
+      console.log("lo sentimos no hay stock");
     }
+  } catch (err) {
+    console.log(err);
   }
-  setDellCar() {
-    delete this.carrito;
-    this.productos = 0;
-    this.contenedor = {};
-    this.total = 0;
-  }
-  async setDellCarById(id) {
-    const elementoEliminado = await this.carritos.deleteById(id);
-    delete this.contenedor[id];
-    console.log(elementoEliminado);
-  }
+}
+setDellCar() {
+  delete this.carrito;
+  this.productos = 0;
+  this.contenedor = {};
+  this.total = 0;
+}
+async setDellCarById(id) {
+  const elementoEliminado=await this.carritos.deleteById(id);
+  delete this.contenedor[id];
+  console.log(elementoEliminado);
+}
 
-  async getAllCar(id) {
-    const datos = await this.carritos.getById(id);
+async getAllCar(id) {
+      const datos=await this.carritos.getById(id);
 
-    return datos;
-  }
-  getProductosTotal() {
-    return this.productos;
-  }
-  getPrecioTotal() {
-    return this.total;
-  }
+return datos;
+}
+getProductosTotal() {
+  return this.productos;
+}
+getPrecioTotal() {
+  return this.total;
+}
 }
 
 export default Carrito;
--- src/routes/api/carrito/carritoServidor.js
+++ src/routes/api/carrito/carritoServidor.js
@@ -1,52 +1,42 @@
-import  Carrito from "../carrito/carrito.js";
-import  { Router } from'express';
-import   express  from'express';
-import { fileURLToPath } from 'url';
-import path from 'path';
+import Carrito from "../carrito/carrito.js";
+import { Router } from "express";
+import express from "express";
+import { fileURLToPath } from "url";
+import path from "path";
 const __filename = fileURLToPath(import.meta.url);
-const _dirname = path.dirname(_filename);
-
+const __dirname = path.dirname(__filename);
 const router = Router();
 const carrito = new Carrito();
-const datosAgregados={};
-
-
+const datosAgregados = {};
 
 //GET '/api/productos' -> devuelve todos los productos.
 router.use(express.json());
 
-router.post("/",async (req, res) => {
-
+router.post("/", async (req, res) => {
   try {
+    const id = req.body.id;
+    const cantidad = req.body.cantidad;
 
-          const id =req.body.id;
-          const cantidad=req.body.cantidad;
-
-        const valores = await carrito.setAddCarAsync(id,cantidad)
+    const valores = await carrito.setAddCarAsync(id, cantidad);
 
     res.status(200).json(valores);
 
-
     //res.status(200).render('partials/producto',{articulo: await articulos.save(producto)})
-
   } catch (err) {
-    console.error( err);
-    res.status(400).json({error: err.toString()});
+    console.error(err);
+    res.status(400).json({ error: err.toString() });
   }
 });
-
 
 router.delete("/:id", async (req, res) => {
   try {
-    const respuesta = await carrito.setDellCarById(Number(req.params.id))
+    const respuesta = await carrito.setDellCarById(Number(req.params.id));
     res.status(200).json(respuesta);
   } catch (err) {
-    console.error( err);
+    console.error(err);
     res.status(400).json({ error: "datos incorrectos" });
   }
 });
-
-
 
 //GET '/api/productos/:id' -> devuelve un producto según su id.
 router.get("/:id/productos", async (req, res) => {
@@ -57,18 +47,14 @@ router.get("/:id/productos", async (req, res) => {
     if (result === null) {
       throw new Error("No Existe el producto");
     } else {
-
-      res.status(200).json({articulo: await result})
+      res.status(200).json({ articulo: await result });
     }
   } catch (err) {
-    console.error( err);
-    res.status(400).json({error: err.toString()});
+    console.error(err);
+    res.status(400).json({ error: err.toString() });
   }
   //
 });
-
-
-
 
 //PUT '/api/productos/:id' -> recibe y actualiza un producto según su id.
 router.put("/:id", async (req, res) => {
@@ -77,10 +63,9 @@ router.put("/:id", async (req, res) => {
     producto.id = req.params.id;
     res.status(200).json(await articulos.updateById(producto));
   } catch (err) {
-    console.error( err);
+    console.error(err);
     res.status(400).send({ error: "datos incorrectos" });
   }
 });
-
 
 export default router;
